5003b4e457425ad5edd640c859afad81aa7e3c75
Murali0314 <gorlamuralidhar@gmail.com>
2025-08-10 22:42:54 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: "/BookNest/",
   plugins: [react()],
-})
+  base: '/', // Serve assets relative to the root path
+  build: {
+    outDir: 'dist', // output folder (default is 'dist')
+    sourcemap: false, // disable source maps for production (optional)
+    // You can add more build options here if needed
+  },
+});
+
